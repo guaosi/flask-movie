5680d557b81864c3ae8405b13935a3efdb9d46e5
guaosi <guaosi@vip.qq.com>
2018-07-04 22:02:25 +0800

    add bullet by redis

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,11 @@
 from flask import Flask
-from flask_wtf.csrf import CSRFProtect
+from flask_wtf.csrf import CSRFProtect, CsrfProtect
 from app.models.base import db
 from flask_login import LoginManager
+from flask_redis import FlaskRedis
 login_manager = LoginManager()
+rd = FlaskRedis()
+csrf = CSRFProtect()
 def create_app():
     app=Flask(__name__)
     app.config.from_object('app.config.secure')
@@ -12,9 +15,11 @@ def create_app():
     login_manager.login_message='请先进行登陆'#没有登录时候的闪现信息
     login_manager.login_message_category='err'
     register_blueprint(app)
+    # 注册redis
+    rd.init_app(app)
     register_db(app)
     # csrf保护
-    CSRFProtect(app)
+    csrf.init_app(app)
     return app
 def register_blueprint(app):
     from app.home import create_home_blueprint as r1
--- app/home/main.py
+++ app/home/main.py
@@ -1,9 +1,9 @@
+import datetime
+import uuid
 from threading import Thread
-
-from flask import render_template, request, current_app, flash, redirect, url_for, jsonify
+from flask import render_template, request, current_app, flash, redirect, url_for, jsonify, Response
 from flask_login import current_user, login_required
-
-from app import db
+from app import db, rd, csrf
 from app.libs.redprint import RedPrint
 from app.models.comment import Comment
 from app.models.movie import Movie
@@ -55,6 +55,7 @@ def search(page=None):
     key=request.args.get('key','')
     movies=Movie.query.filter(Movie.title.like('%'+key+'%')).paginate(page,per_page=current_app.config['HOME_SEARCH_PAGE'])
     return render_template('home/search.html',movies=movies,key=key)
+#普通逻辑
 @app.route('/play/<int:id>/<int:page>',methods=['GET','POST'])
 def play(id,page=None):
     movie=Movie.query.get_or_404(id)
@@ -79,7 +80,85 @@ def play(id,page=None):
         thread.start()
         return redirect(url_for('home.play', id=form.movie_id.data,page=page))
     return render_template('home/play.html',movie=movie,form=form,comments=comments)
+# 弹幕逻辑
+@app.route('/video/<int:id>/<int:page>',methods=['GET','POST'])
+def video(id,page=None):
+    movie=Movie.query.get_or_404(id)
+    if page is None:
+        page=1
+    comments=Comment.query.filter_by(movie_id=id).order_by(Comment.addtime.desc()).paginate(page,per_page=current_app.config['HOME_COMMENT_PAGE'])
+    app = current_app._get_current_object()
+    thread=Thread(target=async_add_playnum,args=[id,app])
+    thread.start()
+    form = CommentAddForm(request.form)
+    if request.method == 'POST' and form.validate():
+        if not current_user.is_authenticated:
+            return redirect(url_for('home.login'))
+        value = form.data
+        value['user_id'] = current_user.id
+        comment = Comment()
+        comment.set_attr(value)
+        with db.auto_commit():
+            db.session.add(comment)
+        flash('添加评论成功~', 'ok')
+        thread=Thread(target=async_add_commentnum,args=[form.movie_id.data,app])
+        thread.start()
+        return redirect(url_for('home.video', id=form.movie_id.data,page=page))
+    return render_template('home/video.html',movie=movie,form=form,comments=comments)
 
+@csrf.exempt
+@app.route("/tm", methods=["GET", "POST"])
+def tm():
+    """
+    弹幕消息处理
+    """
+    import json
+    if request.method == "GET":
+        # 获取弹幕消息队列
+        id = request.args.get('id')
+        # 存放在redis队列中的键值
+        key = "movie" + str(id)
+        if rd.llen(key):
+            msgs = rd.lrange(key, 0, 2999)
+            res = {
+                "code": 1,
+                "danmaku": [json.loads(v) for v in msgs]
+            }
+        else:
+            res = {
+                "code": 1,
+                "danmaku": []
+            }
+        resp = json.dumps(res)
+    if request.method == "POST":
+        # 添加弹幕
+        data = json.loads(request.get_data())
+        msg = {
+            "__v": 0,
+            "author": data["author"],
+            "time": data["time"],
+            "text": data["text"],
+            "color": data["color"],
+            "type": data['type'],
+            "ip": request.remote_addr,
+            "_id": datetime.datetime.now().strftime("%Y%m%d%H%M%S") + uuid.uuid4().hex,
+            "player": [
+                data["player"]
+            ]
+        }
+        res = {
+            "code": 1,
+            "data": msg
+        }
+        resp = json.dumps(res)
+        # 将添加的弹幕推入redis的队列中
+        rd.lpush("movie" + str(data["player"]), json.dumps(msg))
+        print('发送')
+    return Response(resp, mimetype='application/json')
+
+
+
+# 异步任务
 @app.route('/moviecol',methods=['POST'])
 @login_required
 def add_moviecol():
@@ -116,6 +195,8 @@ def async_add_commentnum(id,app):
             db.session.commit()
         except Exception as e:
             db.session.rollback()
+
+
 
 
 
